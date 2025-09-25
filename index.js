35707651303cd61d27d7364727a2a6b62a306fa2
yvvonne <yvonnekajuju6@gmail.com>
2025-09-25 20:55:16 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.getElementById("text");
+  textElement.textContent = "This is really cool!";
+});
